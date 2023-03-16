62a5a31709d146a53bfe33c87958a3a724916534
Maikel Rodrigues <srodgon@live.com>
2023-03-16 17:33:49 +0100

    Material sidenav added

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MaterialModule } from '../material/material.module';
+import { HeroesRoutingModule } from './heroes-routing.module';
+
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
-import { HeroesRoutingModule } from './heroes-routing.module';
+
+
+
 
 
 
@@ -19,7 +26,9 @@ import { HeroesRoutingModule } from './heroes-routing.module';
   ],
   imports: [
     CommonModule,
-    HeroesRoutingModule
+    HeroesRoutingModule,
+    FlexLayoutModule,
+    MaterialModule
   ]
 })
 export class HeroesModule { }
